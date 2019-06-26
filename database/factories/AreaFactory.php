5bcaa75ca1b46de9f969ee5920186afa3372628b
Richard Taicus Moriano <richardtmoriano94@gmail.com>
2019-06-25 20:42:00 -0500

    tablas area. carrrera y lengua

---
+++ database/factories/AreaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\area;
+use Faker\Generator as Faker;
+
+$factory->define(area::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
