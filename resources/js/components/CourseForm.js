ab03278bfcce8b8e8586e62add7da585c3971645
Darwin <salinash2000@gmail.com>
2019-07-22 22:23:17 -0600

    Handle state form with hooks

--- resources/js/components/CourseForm.js
+++ resources/js/components/CourseForm.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
-export default class CourseForm extends Component {
+class CourseForms extends Component {
     state = {
         curso: '',
         descripcion: '',
@@ -94,4 +94,94 @@ export default class CourseForm extends Component {
             </div>
         )
     }
+}
+
+export default function CourseForm() {
+    const [curso, setCurso] = useState({})
+
+    const onChange = (e) => {
+        setCurso({
+            ...curso,
+            [e.target.name]: e.target.value
+        })
+        console.log(curso)
+    }
+
+    return (
+        <div className="card mt-5 py-5">
+            <div className="card-body">
+                <h2 className="card-title text-center mb-5">
+                    Nuevo Curso
+                    </h2>
+                <form>
+                    <div className="form-group row">
+                        <label htmlFor="" className="col-sm-4 col-lg-2 col-form-label">
+                            Nombre del curso
+                            </label>
+                        <div className="col-sm-8 col-lg-10">
+                            <input
+                                onChange={onChange}
+                                name="curso"
+                                type="text"
+                                className="form-control  "
+                                placeholder="Nombre del curso"
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="" className="col-sm-4 col-lg-2 col-form-label">
+                            Descripción
+                            </label>
+                        <div className="col-sm-8 col-lg-10">
+                            <input
+                                onChange={onChange}
+                                name="descripcion"
+                                type="text"
+                                className="form-control  "
+                                placeholder="Descripcion del curso"
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="" className="col-sm-4 col-lg-2 col-form-label">
+                            Imagen
+                            </label>
+                        <div className="col-sm-8 col-lg-4">
+                            <input
+                                onChange={onChange}
+                                name="imagen"
+                                type="text"
+                                className="form-control  "
+                            />
+                        </div>
+                        <label htmlFor="" className="col-sm-4 col-lg-2 col-form-label">
+                            Lengua
+                            </label>
+                        <div className="col-sm-8 col-lg-4">
+                            <input
+                                onChange={onChange}
+                                name="lengua"
+                                type="text"
+                                className="form-control  "
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="" className="col-sm-4 col-lg-2 col-form-label">
+                            Desarrollador
+                            </label>
+                        <div className="col-sm-8 col-lg-10">
+                            <input
+                                onChange={onChange}
+                                name="desarrollador"
+                                className="form-control "
+                                placeholder="Desarrollador"
+                            />
+                        </div>
+                    </div>
+                    <input type="submit" className="btn btn-success btn-block py-3 mt-2" value="Guardar" />
+                </form>
+            </div>
+        </div>
+    )
 }
